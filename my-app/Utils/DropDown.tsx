248b3720c52402342e68edd10911b09400ebf948
ayxan65 <memmedliayxan0@gmail.com>
2024-07-25 09:38:43 +0400

    added

---
+++ my-app/Utils/DropDown.tsx
@@ -0,0 +1,50 @@
+"use client"
+import React, {useState} from 'react';
+import Image from 'next/image';
+import arrow from '../public/DashBoard/arrow.svg';
+
+const DropDown = () => {
+    const [isOpen, setOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState<string>('This Week');
+    const [options, setOptions] = useState<string[]>(['This Week', 'Today', 'This Month', 'This Year']);
+  
+    const selectOption = (option: string) => {
+      setSelectedOption(option);
+      setOptions([selectedOption, ...options.filter(opt => opt !== option)]);
+    };
+
+    const toggleDropDown = () => {
+        setOpen(!isOpen);
+    }
+
+  return (
+    <div>
+        <div className='flex  '>
+        <button className='text-lg mx-2' onClick={toggleDropDown}>{selectedOption} </button>
+      
+       <Image alt='arrow' src={arrow}></Image>
+       
+        </div>
+        {
+            isOpen && (
+              
+               <div  >
+                {options.map(option => (
+          <button
+            key={option}
+            className="block p-2"
+            onClick={() => selectOption(option)}
+          >
+            {option}
+          </button>
+        ))}
+             
+               
+               </div>
+            )
+        }
+    </div>
+  )
+}
+
+export default DropDown
--- my-app/components/Home/Activity.tsx
+++ my-app/components/Home/Activity.tsx
@@ -1,17 +1,23 @@
-import ThisWeek from '@/Utils/ThisWeek'
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import DropDown from '../../Utils/DropDown';
+import Task from '../../public/Activity/task.svg';
+import graph from '../../public/Activity/Graph.svg';
 
 const Activity = () => {
   return (
-    <div className='h-[10vh] w-10/12 bg-active   rounded-xl'>
+    <div className='h-[34vh] w-10/12 bg-active   rounded-xl'>
         <div className='p-5'>
             <div className=' flex justify-between '><div className='font-semibold text-xl'>
             Activity</div>
             <div >
-            <div><ThisWeek/></div>
+            <div><DropDown/></div>
               
             </div>
             </div>
+            <Image width={500} className='m-auto mt-7' alt='graph' src={graph}></Image>
+            <Image alt='task' className='bg-red absolute top-10' src={Task} ></Image>
+
         </div>
     </div>
   )
