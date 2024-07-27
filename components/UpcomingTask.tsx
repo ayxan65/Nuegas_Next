5fa549ef8a5de7dc07d765c75ff4d20874c20ee4
ayxan65 <memmedliayxan0@gmail.com>
2024-07-27 22:34:52 +0400

    res

--- components/UpcomingTask.tsx
+++ components/UpcomingTask.tsx
@@ -25,12 +25,12 @@ const UpcomingTask = () => {
     ]
 
   return (
-<div>
+<div className='my-4'>
     <div className='text-xl font-semibold mt-5 '>Upcoming Tasks</div>
- <div  className='block lg:flex lg:justify-between mt-5'>
+ <div  className='h-[100shw] block lg:flex lg:justify-between mt-5'>
  {
        Data.map(({img, time, desc, title}, index) => (
-            <div key={index} className='h-[34swh] lg:w-[26vw] lg:mb-5 rounded-lg  bg-white p-4 my-2 '>
+            <div key={index} className=' lg:w-[26vw] lg:mb-5 rounded-lg  bg-white p-4 my-2 '>
                
                <Image alt='photo' className='pt-2' width={400} src={img}></Image>
                 <p className='mt-4 font-semibold text-black'>{title}</p>
